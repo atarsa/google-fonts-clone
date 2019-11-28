dccf128c10d3f5c0b92adcb10b3a3248a7306367
ATarsa <agnieszkatarsa@gmail.com>
2019-11-28 20:37:44 +0000

    Add basic header

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="brand">
+        Google Fonts
+      </div>
+      <ul>
+        <li>CATALOG</li>
+        <li>FEATURED</li>
+        <li>ARTICLES</li>
+        <li>ABOUT</li>
+      </ul>
+    </header>
+  )
+}
+
+export default Header
