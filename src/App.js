e104b37adb40bad1f15fcabe8b50ad92e51dc155
ATarsa <agnieszkatarsa@gmail.com>
2019-11-28 20:50:02 +0000

    Add basic footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
  return(
    <>
    <Header />
    {/* <Nav />
-   <FontCards />
-   <Footer /> */}
+   <FontCards /> */}
+   <Footer />
    </>
  )
 }
