4603ce8a4f2dfdf53d2c4b226f712c15d080cbb9
ATarsa <agnieszkatarsa@gmail.com>
2019-11-29 18:42:43 +0000

    Add font awsome icons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCircle, faBorderAll, faList, faRedo } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 import Header from './components/Header'
+import FontNav from './components/FontNav'
 import Footer from './components/Footer'
+
+library.add(faCircle, faBorderAll, faList, faRedo)
 
 const App = () => {
  return(
    <>
    <Header />
-   {/* <Nav />
-   <FontCards /> */}
-   <Footer />
+   <main>
+    <FontNav />
+    {/* <FontCards /> */}
+   </main>
+    <Footer />
    </>
  )
 }
